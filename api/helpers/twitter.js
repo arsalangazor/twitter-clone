b48dfb1ab086b8e7e0178252fb8cf83eb2d01702
Arsalan <arsalangazor@icloud.com>
2020-08-04 19:43:19 -0700

    final commit

---
+++ api/helpers/twitter.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const TWITTER_URL = `https://api.twitter.com/1.1/tweets/search/30day/${process.env.TWITTER_ENV_NAME}.json`;
+
+
+class Twitter {
+    get(query, resultType, maxId) {
+        return axios.get(TWITTER_URL, {
+            params: {
+                q: query,
+                result_type: resultType,
+                count: 10,
+                tweet_mode: "extended",
+                max_id: maxId
+            },
+            headers: {
+                "Authorization": `Bearer ${process.env.TWITTER_API_TOKEN}`
+            }
+        })
+        
+    }
+
+}
+
+module.exports = Twitter;
